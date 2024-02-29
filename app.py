8c9b73b1d3a6dbf3c170c2e2dd23d4bdec5fc234
stardestory <lachlanrimmington@gmail.com>
2024-03-01 08:46:33 +1000

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,11 @@ number = random.randint(0,int(h))
 while run == True:
     i = input(":")
     if int(i) < number and int(i) > 0:
-        print("to low")
+        print("To low")
     if int(i) > int(h) or int(i) < 0:
         print ("Error number not possible")
     elif int(i) > number and int(i) < int(h):
-        i = int(i)
-    if i < number:
-        print("to low")
-    elif i > number:
-        print("to high")
+        print ("To high")
     else:
         print("correct")
         exit()
