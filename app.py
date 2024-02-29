78f75fc18875f3c378d7f065975a69aefeae1471
rcfulscale <114207070+rcfulscale@users.noreply.github.com>
2024-02-29 12:36:27 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 #imports
-
+import time
+import random
+import math
+import numbers
+run = True
 #runcode
+print("welcome the number wordle")
+number = random.randint(0, 99)
+while run == True:
+    i = input(":")
+    if int(i) < number:
+        print("to low")
+    elif int(i) > number:
+        print("to high")
+    else:
+        print("correct")
+        exit()
