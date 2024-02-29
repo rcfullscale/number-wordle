6dbd1a4e629fd9a2dc8987ed9f5c766c4bbd1304
stardestory <150772561+stardestory@users.noreply.github.com>
2024-02-29 16:42:25 +1000

    Fixed error

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ import numbers
 run = True
 #runcode
 print("welcome the number wordle")
-number = random.randint(0, 99)
+print("Input highest number")
+h = input(":")
+number = random.randint(0,int(h))
 while run == True:
     i = input(":")
-    if int(i) < number:
+    if int(i) < number and int(i) > 0:
         print("to low")
-    elif int(i) > number:
+    if int(i) > int(h) or int(i) < 0:
+        print ("Error number not possible")
+    elif int(i) > number and int(i) < int(h):
         print("to high")
     else:
         print("correct")
