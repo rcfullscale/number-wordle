603b63dad8ba7448346a17ac6bf1284f3f826063
rcfulscale <114207070+rcfulscale@users.noreply.github.com>
2024-02-29 12:56:23 +1000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numbers
 run = True
 #runcode
 print("welcome the number wordle")
-number = random.randint(0, 99)
+number = random.randint(0, 98)
 while run == True:
     i = input(":")
     if int(i) < number:
