17ad83bdc2207cfce7301f0bfc75b4ad4f1cda71
pi270001 <161408137+pi270001@users.noreply.github.com>
2024-03-02 10:45:34 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 #imports
 import time
 import random
+# check each character if it's a number
+def digit (num):
+    return ord(num)>=48 and ord(num)<=57
+# check the whole number at once for non-number characters
+def digits(number):
+    for char in number:
+        if not digit(char):
+            return False
+    return True
 run = True
 #runcode
 print("Welcome the number wordle")
 number = random.randint(0, 100)
 while run == True:
     i = input(":")
-    if int(i) < number:
-        print("to low")
+    if not digits(i):
+        print('this is a "number" game')
     elif int(i) > number:
         print("to high")
+    elif int(i) < number:
+        print("to low")
     else:
         print("correct")
         run=False
