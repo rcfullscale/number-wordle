e4ab256a90992b521ed3900625dfd6960aadc0b1
rcfulscale <114207070+rcfulscale@users.noreply.github.com>
2024-03-03 17:13:57 +1000

    fixed the number bug

--- app.py
+++ app.py
@@ -15,18 +15,24 @@ guess = 1
 print("Welcome to the number wordle")
 print("Input number you want the answer to be below")
 highest_number = input(": ")
-number = random.randint(0,int(h))
+number = random.randint(0,int(highest_number))
 #main
 run = True
 print("Start guessing, this is your first guess")
 while True:
     print("You are on guess " + str(guess))
-    i = input(":")
-    if int(i) < number:
+    while True:
+        i_num = input(":")
+        try:
+            i = int(i_num)
+            break
+        except ValueError:
+            print("there was a letter in your answer")
+    if i < number:
         print("To low")
-    elif int(i) > int(highest_number):
+    elif i > int(highest_number):
         print ("Error number to high")
-    elif int(i) > number:
+    elif i > number:
         print ("To high")
     else:
         print("Correct")
